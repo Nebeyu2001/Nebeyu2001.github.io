d6c0864dc3a975201f02213ccc96a4e322fdbf78
Nebeyu Z <64758297+Nebeyu2001@users.noreply.github.com>
2022-03-08 21:20:39 -0600

    done

---
+++ W3D3/evens.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+function multiplyEvens(...args) {
+
+    let prod = 1;
+
+    for (const argu of arguments) {
+
+        if (arg % 2 === 0) {
+            prod = prod * arg;
+        }
+    }
+
+
+    return prod;
+
+}

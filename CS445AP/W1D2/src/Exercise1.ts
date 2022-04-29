e423d3ff5c35aadfe8564c166370688ea8979332
Nebeyu Z <64758297+Nebeyu2001@users.noreply.github.com>
2022-04-29 16:31:29 -0500

    w2d2

---
+++ CS445AP/W1D2/src/Exercise1.ts
@@ -0,0 +1,16 @@
+"use strict";
+
+class University {
+  name: string;
+  dept: string;
+  constructor(name:string, dept:string) {
+    this.name = name;
+    this.dept = dept;
+  }
+  graduation(year:number):void {
+    console.log(`Graduating ${this.dept} ${year} students`);
+  }
+}
+
+const miu:University = new University("MIU", "MSD");
+miu.graduation(2021);

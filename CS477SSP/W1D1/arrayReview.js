2e44ce14078ca5e5dad8b2dfcef3a3836cc0e459
Nebeyu Z <64758297+Nebeyu2001@users.noreply.github.com>
2022-07-19 13:56:14 -0500

    SSP

---
+++ CS477SSP/W1D1/arrayReview.js
@@ -0,0 +1,56 @@
+"use strict";
+let db = [
+  { id: 1, fname: "John", lname: "Smith" },
+
+  { id: 2, fname: "Lucy", lname: "Jark" },
+
+  { id: 3, fname: "Edward", lname: "Capton" },
+];
+
+class Student {
+  constructor(id, firstname, lastname) {
+    this.id = id;
+
+    this.fname = firstname;
+
+    this.lname = lastname;
+  }
+
+  save() {
+    console.log("this");
+    const stu = db.find((user) => user.id === this.id);
+    if (stu) {
+      throw new Error("student already exists with id" + this.id);
+    } else {
+      db.push(this);
+    }
+  }
+
+  edit() {
+    const stuIndex = db.findIndex((user) => user.id === this.id);
+    if (stuIndex < 0) {
+      throw new Error("student already exists with id" + this.id);
+    }
+    db[stuIndex] = this;
+  }
+
+  static getById(id) {
+    return db.find((user) => user.id === id);
+  }
+
+  static getAll() {}
+
+  deleteById(id) {}
+}
+
+new Student(4, "Tina", "Xing").save(); //save to db
+
+new Student(4, "Miss", "Xing").edit(); //edit studentId with id=4
+
+console.log(db);
+
+// Student.deleteById(4); //remove studentId=4 from db
+
+// Student.getAll(); //return db;
+
+// Student.getById(1); //return {id:1, fname: 'John', lname: 'Smith'}

1c0ff6b93be15c267f56316cde5fee3b9fc00817
Nebeyu Z <64758297+Nebeyu2001@users.noreply.github.com>
2022-07-24 22:43:59 -0500

    "W3D4"

---
+++ CS477SSP/W1D1/test.js
@@ -0,0 +1,19 @@
+// "use strict";
+
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
+
+// http
+//   .createServer(function (req, res) {
+//     fs.readFile("home.html", function (err, data) {
+//       if (err) {
+//         res.end("Whoops");
+//       } else {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.write(data);
+//         return res.end();
+//       }
+//     });
+//   })
+//   .listen(3000, () => console.log("listening 3000"));
